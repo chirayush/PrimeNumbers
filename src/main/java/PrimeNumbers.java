5e8d37e05a12ca3c4071949aee469d8ee7a6523a
Charlie Agarwal <chirayush.agarwal@aa.com>
2018-07-01 12:50:46 -0500

    Refactored to Stream

--- src/main/java/PrimeNumbers.java
+++ src/main/java/PrimeNumbers.java
@@ -1,19 +1,13 @@
+import java.util.stream.IntStream;
+
 final class PrimeNumbers {
     static boolean isPrime(final int input) {
         if (input < 1) {
             throw new IllegalArgumentException("Please enter a number above 0");
         }
 
-        if (input == 2) {
-            return true;
-        }
-
-        for (int i = 2; i < input; i++) {
-            if ((input % i) != 0) {
-                return false;
-            }
-        }
-
-        return true;
+        return IntStream
+                .range(2, input)
+                .noneMatch(i -> (input % i) != 0);
     }
 }
